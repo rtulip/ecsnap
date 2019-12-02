aff149ad0b91b7b9e5aad871387f9c1e1b86159a
Robbie <robert.tulip97@gmail.com>
2019-12-01 16:08:24 -0800

    test: Added a test for remove component. Small cleanup

--- src/world.rs
+++ src/world.rs
@@ -16,6 +16,21 @@ pub struct World {
 
 impl World {
 
+    fn get_component<C: Component>(&self) -> &C::Storage {
+        self.components.get(&TypeId::of::<C>())
+            .unwrap()
+            .downcast_ref::<C::Storage>()
+            .unwrap()
+
+    }
+    
+    fn get_mut_component<C: Component>(&mut self) -> &mut C::Storage {
+        self.components.get_mut(&TypeId::of::<C>())
+            .unwrap()
+            .downcast_mut::<C::Storage>()
+            .unwrap()
+    }
+
     pub fn register_component<C: Component>(&mut self) -> Option<Box<dyn Any>> {
         self.components.insert(TypeId::of::<C>(), Box::new(C::Storage::new()))
     }
@@ -30,25 +45,18 @@ impl World {
     }
     
     pub fn add_component_to_entity<C: Component>(&mut self, entity: Entity, component: C) -> Option<C> {
-        let storage = self.components.get_mut(&TypeId::of::<C>()).unwrap().downcast_mut::<C::Storage>().unwrap();
-        storage.push(entity.id, component)
+        self.get_mut_component::<C>().push(entity.id, component)
     }
     
     pub fn get_component_for_entity<C: Component>(&self, entity: &Entity) -> Option<&C> {
-
-        let storage = self.components.get(&TypeId::of::<C>())
-            .unwrap()
-            .downcast_ref::<C::Storage>()
-            .unwrap();
-        storage.get(&entity.id)
+        self.get_component::<C>().get(&entity.id)
     }
     
     pub fn remove_component_from_entity<C: Component>(&mut self, entity: &Entity) -> Option<C> {
-        let storage = self.components.get_mut(&TypeId::of::<C>()).unwrap().downcast_mut::<C::Storage>().unwrap();
-        storage.remove(&entity.id)
+        self.get_mut_component::<C>().remove(&entity.id)
     }
 
-    fn destroy_entity(&mut self, entity: Entity) {}
+    fn destroy_entity(&mut self, entity: &Entity) {}
 
 }
 
@@ -133,5 +141,59 @@ mod test_world {
         assert!(e2_vel.is_none());
 
     }
+
+    #[test]
+    fn test_remove_component_from_entity() {
+
+        #[derive(Debug)]
+        struct Pos {
+            x: f64, 
+            y: f64,
+        }
+
+        #[derive(Debug)]
+        struct Vel {
+            x: f64, 
+            y: f64,
+        }
+
+        impl Component for Pos {
+            type Storage = HashMap<Eid, Self>;
+        }
+
+        impl Component for Vel {
+            type Storage = HashMap<Eid, Self>;
+        }
+
+        let mut world: World = Default::default();
+        world.register_component::<Pos>();
+        world.register_component::<Vel>();
+
+        let e = world.create_entity();
+        
+        world.add_component_to_entity(e, Pos {x: 0.0, y: 0.0});
+        world.add_component_to_entity(e, Vel {x: 0.0, y: 0.0});
+
+        let e_pos = world.get_component_for_entity::<Pos>(&e);
+        let e_vel = world.get_component_for_entity::<Vel>(&e);
+        
+        assert!(e_pos.is_some());
+        assert!(e_pos.unwrap().x == 0.0);
+        assert!(e_pos.unwrap().y == 0.0);
+
+        assert!(e_vel.is_some());
+        assert!(e_vel.unwrap().x == 0.0);
+        assert!(e_vel.unwrap().y == 0.0);
+        
+        let val = world.remove_component_from_entity::<Vel>(&e);
+        assert!(val.is_some());
+        let val = val.unwrap();
+        assert_eq!(val.x, 0.0);
+        assert_eq!(val.y, 0.0);
+
+        let val = world.remove_component_from_entity::<Vel>(&e);
+        assert!(val.is_none());
+    }
+
 
 }
