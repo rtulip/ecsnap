ad5dcd438b47e585036d6ee15c81b10d43ecf583
Robbie <robert.tulip97@gmail.com>
2019-12-02 17:09:46 -0800

    docs: Added documentation to Entity

--- src/entity.rs
+++ src/entity.rs
@@ -2,13 +2,39 @@ use crate::World;
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
 
+/// Type for entity identifier
 pub type Eid = usize;
-#[derive(Default)]
+
+/// `Entity`
+/// A collection for a series of components
+#[derive(Debug, Default)]
 pub struct Entity {
+    /// A Hashmap used to store the components of the entities.
     pub components: HashMap<TypeId, Box<dyn Any>>,
 }
 
 impl Entity {
+    /// Adds a component C to the `Entity`. If the `Entity` didn't have a component of
+    /// type C already, None is returned. If the `Entity` already had a component of type
+    /// C, the component is replaced and the old Boxed component is retured.
+    ///
+    /// # Example
+    /// ```
+    /// extern crate ecsnap;
+    /// use ecsnap::Entity;
+    ///
+    /// struct Pos {
+    ///     x: f64,
+    ///     y: f64,
+    /// }
+    ///
+    /// let mut e = Entity::default();
+    /// e.add_component(Pos { x: 0.0, y: 0.0 });
+    ///
+    /// let pos = e.get_component::<Pos>().unwrap();
+    /// assert_eq!(pos.x, 0.0);
+    /// assert_eq!(pos.y, 0.0);
+    /// ```
     pub fn add_component<C: 'static>(&mut self, component: C) -> Option<Box<C>> {
         if let Some(bx) = self
             .components
@@ -24,6 +50,26 @@ impl Entity {
         }
     }
 
+    /// Gets a reference to a component C for an `Entity` if the `Entity` has such a
+    /// component, otherwise None is returned.
+    ///
+    /// # Example
+    /// ```
+    /// extern crate ecsnap;
+    /// use ecsnap::Entity;
+    ///
+    /// struct Pos {
+    ///     x: f64,
+    ///     y: f64,
+    /// }
+    ///
+    /// let mut e = Entity::default();
+    /// e.add_component(Pos { x: 0.0, y: 0.0 });
+    ///
+    /// let pos = e.get_component::<Pos>().unwrap();
+    /// assert_eq!(pos.x, 0.0);
+    /// assert_eq!(pos.y, 0.0);
+    /// ```
     pub fn get_component<C: 'static>(&self) -> Option<&C> {
         if let Some(bx) = self.components.get(&TypeId::of::<C>()) {
             bx.downcast_ref::<C>()
@@ -32,6 +78,26 @@ impl Entity {
         }
     }
 
+    /// Gets a component C for an `Entity`. If the `Entity` has a Component C, a
+    /// reference to the component is returned, otherwise None is returned.
+    ///
+    /// # Example
+    /// ```
+    /// extern crate ecsnap;
+    /// use ecsnap::Entity;
+    ///
+    /// struct Pos {
+    ///     x: f64,
+    ///     y: f64,
+    /// }
+    ///
+    /// let mut e = Entity::default();
+    /// e.add_component(Pos { x: 0.0, y: 0.0 });
+    ///
+    /// let pos = e.get_component::<Pos>().unwrap();
+    /// assert_eq!(pos.x, 0.0);
+    /// assert_eq!(pos.y, 0.0);
+    /// ```
     pub fn get_mut_component<C: 'static>(&mut self) -> Option<&mut C> {
         self.components
             .get_mut(&TypeId::of::<C>())
@@ -39,6 +105,25 @@ impl Entity {
             .downcast_mut::<C>()
     }
 
+    /// Removes a component C from an `Entity` if it had such a component. If it had a
+    /// component C, then a Boxed<C> is returned, otherwise, None is returned.
+    ///
+    /// # Example
+    /// ```
+    /// extern crate ecsnap;
+    /// use ecsnap::Entity;
+    ///
+    /// struct Pos {
+    ///     x: f64,
+    ///     y: f64,
+    /// }
+    ///
+    /// let mut e = Entity::default();
+    /// e.add_component(Pos { x: 0.0, y: 0.0 });
+    /// let pos = e.remove_component::<Pos>().unwrap();
+    /// assert_eq!((*pos).x, 0.0);
+    /// assert_eq!((*pos).y, 0.0);
+    /// ```
     pub fn remove_component<C: 'static>(&mut self) -> Option<Box<C>> {
         if let Some(bx) = self.components.remove(&TypeId::of::<C>()) {
             if let Ok(comp) = bx.downcast::<C>() {
