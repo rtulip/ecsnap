9ac3aba5864da1a4bd6b58707cdbf2bd8329330f
Robbie <robert.tulip97@gmail.com>
2019-12-05 01:48:41 -0800

    test: Updated system tests to do a multi-query

--- src/system.rs
+++ src/system.rs
@@ -41,23 +41,36 @@ mod test_system {
             x: f64,
             y: f64,
         }
+
+        #[derive(Debug, Clone)]
+        struct Vel {
+            x: f64,
+            y: f64,
+        }
         impl Component for Pos {}
+        impl Component for Vel {}
 
         let mut world = World::default();
-        world.create_entity().with(Pos { x: 0.0, y: 0.0 }).build();
+        world
+            .create_entity()
+            .with(Pos { x: 0.0, y: 0.0 })
+            .with(Vel { x: 1.6, y: -4.5} )
+            .build();
 
         struct ReadSys {}
 
         impl<'a> System<'a> for ReadSys {
-            type Data = Query<'a, Pos>;
+            type Data = (Query<'a, Pos>, Query<'a, Vel>);
 
             fn run(&mut self, data: &mut Self::Data) {
-                println!("Pos: {:?}", data);
+                let (pos, vel) = data;
+                println!("Pos: {:?}", pos);
+                println!("Vel: {:?}", vel);
             }
         }
 
         let mut rs = ReadSys {};
-        
+        println!("Dispatching System!");
         world.dispatch_system(&mut rs);
     }
 
