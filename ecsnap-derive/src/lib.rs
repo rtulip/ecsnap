8876b55cd336581c273ba2555be47f57588570d4
Robbie <robert.tulip97@gmail.com>
2019-12-09 11:50:26 -0800

    feat: Added Component macro

--- ecsnap-derive/src/lib.rs
+++ ecsnap-derive/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+
+use crate::proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(Component)]
+pub fn component_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_component(&ast)
+}
+
+fn impl_component(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl Component for #name {}
+    };
+    gen.into()
 }
